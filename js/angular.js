369018920d2f3b43a7dea1e13bc97571ae4bf49a
Dmitry Romanchuk <zmitrok95@gmail.com>
2017-10-09 19:04:14 +0300

    Update angular.js

--- js/angular.js
+++ js/angular.js
@@ -4,13 +4,13 @@ function ngAppExampleController($scope) {
     $scope.button = '';
     $scope.data = {
         done: false,
-        newTodo: 'Do something!',
+        newTodo: '',
         created: new Date(),
         updated: null,
         deleted: null,
-        todo: JSON.parse(localStorage.getItem('list'))||[],
-        archive: JSON.parse(localStorage.getItem('archive'))||[],
-        check: JSON.parse(localStorage.getItem('done'))||[]
+        todo: JSON.parse(localStorage.getItem('list')) || [],
+        archive: JSON.parse(localStorage.getItem('archive')) || [],
+        check: JSON.parse(localStorage.getItem('check')) || []
     };
     $scope.addItem = function () {
         $scope.data.todo.push({
@@ -23,7 +23,7 @@ function ngAppExampleController($scope) {
         $scope.data.todo = $scope.data.todo.filter(function (todos) {
             if (todos.done) {
                 $scope.data.archive.push(todos);
-                todos.done=false;
+                todos.done = false;
                 return false;
             } else {
                 return true;
@@ -43,11 +43,20 @@ function ngAppExampleController($scope) {
         localStorage.setItem('list', JSON.stringify($scope.data.todo));
         localStorage.setItem('archive', JSON.stringify($scope.data.archive));
     };
-    $scope.checkTodo = function (todo) {
+    $scope.checkItem = function (todo) {
         var arr = $scope.data.todo.splice($scope.data.todo.indexOf(todo), 1);
         $scope.data.check = $scope.data.check.concat(arr);
         console.log($scope.button);
         localStorage.setItem('list', JSON.stringify($scope.data.todo));
-        localStorage.setItem('done', JSON.stringify($scope.data.check));
+        localStorage.setItem('check', JSON.stringify($scope.data.check));
+    };
+    $scope.removeAll = function (todo) {
+        if (todo === $scope.data.archive) {
+            $scope.data.archive = [];
+            localStorage.removeItem('archive');
+        } else if (todo === $scope.data.check) {
+            $scope.data.check = [];
+            localStorage.removeItem('check');
+        }
     };
 }
