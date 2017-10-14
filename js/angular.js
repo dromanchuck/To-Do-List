2b1cb004e12a5b089b803bacf2093034de4e64da
Dmitry Romanchuk <zmitrok95@gmail.com>
2017-10-14 08:20:57 +0300

    Update angular.js

--- js/angular.js
+++ js/angular.js
@@ -6,8 +6,8 @@ function ngAppExampleController($scope, $timeout) {
         done: false,
         newTodo: '',
         created: new Date(),
-        updated: new Date(),
-        deleted: new Date(),
+        updated:'',
+        deleted: '',
         todo: JSON.parse(localStorage.getItem('list')) || [],
         archive: JSON.parse(localStorage.getItem('archive')) || [],
         check: JSON.parse(localStorage.getItem('check')) || []
@@ -57,22 +57,19 @@ function ngAppExampleController($scope, $timeout) {
             $scope.data.check = $scope.data.check.concat(arr);
             localStorage.setItem('list', JSON.stringify($scope.data.todo));
             localStorage.setItem('check', JSON.stringify($scope.data.check));
-        }, 600);
+        }, 900);
     };
-    $scope.removeItemForever = function (todo) {
-        todo.className = 'animated bounceOut';
-        $timeout(function () {
-            $scope.data.archive.splice($scope.data.archive.indexOf(todo), 1);
-            localStorage.setItem('archive', JSON.stringify($scope.data.archive));
-        }, 600);
-    };
-    $scope.removeAll = function (todo) {
-        if (todo === $scope.data.archive) {
-            $scope.data.archive = [];
-            localStorage.removeItem('archive');
-        } else if (todo === $scope.data.check) {
-            $scope.data.check = [];
-            localStorage.removeItem('check');
+    $scope.removeItemForever = function (todo,arr) {
+        if (arr === $scope.data.archive) {
+            todo.className = 'animated bounceOut';
+            $timeout(function () {
+                $scope.data.archive.splice($scope.data.archive.indexOf(todo), 1);
+                localStorage.setItem('archive', JSON.stringify($scope.data.archive));
+            }, 900);
+        } else {
+            console.log('See your progress, don\'t delete)');
         }
     };
+    
+  
 }
